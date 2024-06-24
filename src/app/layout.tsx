1050b10f607d25989a2629c427df6e36d19ea115
orangejuice <cherrymeteor@outlook.com>
2024-06-24 16:40:56 +0100

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,12 @@ const inter = Inter({subsets: ["latin"]})
 
 export const metadata: Metadata = {
   title: "PhilCare Ireland",
-  description: "Bridging healthcare across continents, empowering Irish nursing homes with exceptional Filipino talent."
+  description: "Bridging healthcare across continents, empowering Irish nursing homes with exceptional Filipino talent.",
+  icons: {
+    icon: [{url: "/favicon-16x16.png", sizes: "16x16"}, {url: "/favicon-32x32.png", sizes: "32x32"}],
+    apple: "/apple-touch-icon.png"
+  },
+  manifest: "/site.webmanifest"
 }
 
 export default function RootLayout({children}: Readonly<{children: ReactNode}>) {

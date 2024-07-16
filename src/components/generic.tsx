07cff809e72adb5e90f82233ce4ec199eb4d3e16
orangejuice <cherrymeteor@outlook.com>
2024-07-16 10:42:31 +0100

    format

--- src/components/generic.tsx
+++ src/components/generic.tsx
@@ -1,6 +1,6 @@
 import {MotionProps} from "framer-motion"
 
 export const viewPortFadeIn = (delay = 0) => ({
-    initial: "hidden", whileInView: "visible", viewport: {once: true},
-    variants: {hidden: {opacity: 0, y: 20}, visible: {opacity: 1, y: 0, transition: {duration: 0.6, delay}}}
-  } as MotionProps)
+  initial: "hidden", whileInView: "visible", viewport: {once: true},
+  variants: {hidden: {opacity: 0, y: 20}, visible: {opacity: 1, y: 0, transition: {duration: 0.6, delay}}}
+} as MotionProps)
